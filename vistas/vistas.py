3702fbad9a5c07a8eaed5be5ee9779a9202b37a7
Oscar Fonseca <o.fonsecan@uniandes.edu.co>
2024-11-01 20:57:08 -0500

    Test new quality gate 2

--- vistas/vistas.py
+++ vistas/vistas.py
@@ -341,6 +341,12 @@ class VistaReceta(Resource):
         db.session.delete(receta)
         db.session.commit()
         return "", 204
+    
+    def pruebaqualitygate2(self, id_receta):
+        receta = Receta.query.get_or_404(id_receta)
+        db.session.delete(receta)
+        db.session.commit()
+        return "", 204
 
     def borrar_ingrediente_util(self, receta_ingredientes, receta_ingrediente):
         borrar = True
